0621a6da0d9b2818e531e3ae7d717be29eef547d
unknown <bcbf21@gmail.com>
2021-10-25 17:48:46 -0500

    Ready for submission

--- src/__tests__/EnterWanted.test.ts
+++ src/__tests__/EnterWanted.test.ts
@@ -1,8 +1,20 @@
+import { SpecPage } from "./PagePractice";
 const chromedriver = require("chromedriver");
-import { WebDriver, Builder, Capabilities } from "selenium-webdriver";
+import {WebDriver, Builder, Capabilities, By } from "selenium-webdriver";
+
 const driver: WebDriver = new Builder()
   .withCapabilities(Capabilities.chrome())
   .build();
+const page = new SpecPage(driver);
 
-  import { PracticePage } from "./PracticePage";
+//testing header's character count of 9-19 characters to 20 characters
+test("Header", async () => {
+  await page.navigate();
+  await page.doSearch("DUI Violation Per WI");
+});
+
+afterAll(async () => {
+  await driver.quit();
+});
   
+//Jira link: https://dmutah.atlassian.net/jira/software/c/projects/BB5DL/issues/BB5DL-37?jql=project%20%3D%20%22BB5DL%22%20AND%20text%20~%20%22enter%20wanted%22%20ORDER%20BY%20created%20DESC
--- src/__tests__/PagePractice.ts
+++ src/__tests__/PagePractice.ts
@@ -1,74 +1,45 @@
-import { By, until, WebDriver } from "selenium-webdriver";
+import { WebDriver, By, until } from "selenium-webdriver";
 
-/**
- * PracticePage is a page object pointing to Medium.com's topic page.
- * It has methods to navigate there, to search, and to pull article titles.
- */
-export class PracticePage {
-  /** the page's driver object */
-  driver: WebDriver;
-  /** https://medium.com/topics */
-  url: string = "https://devmountain-qa.github.io/enter-wanted/1.4_Assignment/index.html";
-  /** the header marking that we are on the topics page */
-  header: By = By.xpath("");
-  /** the button to open the search input */
-  searchButton: By = By.css('[title="Search Medium"]');
-  /** the titles in the search results */
-  stories: By = By.css(".graf--title");
-  /**
-   * @param {WebDriver} driver - the driver object the page object should interact with
-   * @example
-   * const page = new PracticePage(driver);
-   */
-  constructor(driver: WebDriver) {
-    this.driver = driver;
-  }
-  /**
-   * Will navigate to https://medium.com/topics
-   * @example
-   * await page.navigate();
-   */
-  async navigate() {
-    await this.driver.get(this.url);
-    await this.driver.wait(until.elementLocated(this.header));
-    await this.driver.wait(
-      until.elementIsVisible(await this.driver.findElement(this.header))
-    );
-  }
-  /**
-   * Will open the searchbox and input the search terms, hitting enter
-   * @param {string} searchTerm - The word/phrase you want to search
-   * @example
-   * await page.searchStories("Test Automation");
-   */
-  async searchStories(searchTerm: string) {
-    await click(this.driver, this.searchButton);
-    await this.driver.switchTo().activeElement().sendKeys(`${searchTerm}\n`);
-    await this.driver.wait(until.elementLocated(this.stories));
-  }
-
-  /**
-   * Will return an array of strings; the titles that appeared as a result of
-   * the user's search. Note: The titles are returned lower case.
-   * @example
-   * let titles = await page.getStoryTitles();
-   */
-  async getStoryTitles() {
-    let titles = [];
-    await this.driver.wait(until.elementsLocated(this.stories));
-    let elements = await this.driver.findElements(this.stories);
-    for (let i = 0; i < elements.length; i++) {
-      titles.push(await (await elements[i].getText()).toLowerCase());
+export class SpecPage {
+    driver: WebDriver;
+    // navigation added Enter Wanted webpage url
+    url: string = "https://devmountain-qa.github.io/enter-wanted/1.4_Assignment/index.html";
+  //selectors from Enter Wanted for Header and Name input fields
+    headerBar: By = By.name("hdrInput");
+    nameBar: By = By.name("namInput");
+    
+  
+    //I heavely relied on the solution for this from previous lab
+    //I am going to try to understand what each of these mean
+    constructor(driver: WebDriver) {
+      this.driver = driver;
     }
-    return titles;
-  }
-}
+    async navigate() {
+        //get Enter Wanted's url
+      await this.driver.get(this.url);
+      //locate Search bar
+      await this.driver.wait(until.elementLocated(this.headerBar));
+      //this section is for it to find te search bar again?
+      await this.driver.wait(
+        until.elementIsVisible(await this.driver.findElement(this.headerBar))
+      );
+    }
+  //these two following sections are to do a search on the search bar
+    async sendKeys(elementBy: By, keys) {
+      await this.driver.wait(until.elementLocated(elementBy));
+      return this.driver.findElement(elementBy).sendKeys(keys);
+    }
+  
+    async getText(elementBy: By) {
+      await this.driver.wait(until.elementLocated(elementBy));
+      return (await this.driver.findElement(elementBy)).getText();
+    }
+  //this is for it to type the string we are going to be searching
+    async doSearch(text: string) {
+      return this.sendKeys(this.headerBar, `${text}\n`);
+    }
+  
+    }
+  
+  
 
-const sendKeys = async function (driver, elementBy: By, keys) {
-  await driver.wait(until.elementLocated(elementBy));
-  return driver.findElement(elementBy).sendKeys(keys);
-};
-const click = async function (driver, elementBy: By) {
-  await driver.wait(until.elementLocated(elementBy));
-  return (await driver.findElement(elementBy)).click();
-};
