3412b5c4553af4e2bb4622ef676d8a3e8c47c382
unknown <bcbf21@gmail.com>
2021-10-25 16:23:59 -0500

    Ready to push

---
+++ src/__tests__/EnterWanted.test.ts
@@ -0,0 +1,8 @@
+const chromedriver = require("chromedriver");
+import { WebDriver, Builder, Capabilities } from "selenium-webdriver";
+const driver: WebDriver = new Builder()
+  .withCapabilities(Capabilities.chrome())
+  .build();
+
+  import { PracticePage } from "./PracticePage";
+  
